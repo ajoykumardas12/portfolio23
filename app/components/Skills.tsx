d3bf3df28e6a102fc40e0149a3cb76d40f282778
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-06-08 14:38:01 +0530

    skills structure

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,3 +1,54 @@
+import CSSIcon from "./icons/skills/CSSIcon";
+import Git from "./icons/skills/Git";
+import Java from "./icons/skills/Java";
+import JavaScript from "./icons/skills/JavaScript";
+import NextJS from "./icons/skills/NextJS";
+import NodeJS from "./icons/skills/NodeJS";
+import ReactIcon from "./icons/skills/ReactIcon";
+import TailwindCSS from "./icons/skills/TailwindCSS";
+import TypeScript from "./icons/skills/TypeScript";
+import VSCode from "./icons/skills/VSCode";
+
+const skillItems = [
+  { name: "JavaScript", Icon: JavaScript },
+  {
+    name: "TypeScript",
+    Icon: TypeScript,
+  },
+  {
+    name: "React",
+    Icon: ReactIcon,
+  },
+  {
+    name: "Next.js",
+    Icon: NextJS,
+  },
+  {
+    name: "CSS",
+    Icon: CSSIcon,
+  },
+  {
+    name: "Tailwind CSS",
+    Icon: TailwindCSS,
+  },
+  {
+    name: "Node.js",
+    Icon: NodeJS,
+  },
+  {
+    name: "Java",
+    Icon: Java,
+  },
+  {
+    name: "Git",
+    Icon: Git,
+  },
+  {
+    name: "VS Code",
+    Icon: VSCode,
+  },
+];
+
 function Skills() {
   return (
     <div className="w-[87%] xs:w-[85%] sm:w-[78%] lg:w-[72%] xl:w-8/12 py-20 flex flex-col justify-center">
@@ -9,35 +60,25 @@ function Skills() {
         in blogger where I first experienced few bits of magic of CSS. Since
         then my skills have grown as I kept exploring.
       </p>
-      <p className="text-lg">
-        Here are a few technologies I enjoy working with:
+      <p className="text-lg mb-6">
+        Here are a few technologies of I enjoy working with:
       </p>
-      <div className="grid">
-        <div>
-          <h3>Languages</h3>
-          <ul>
-            <li>JavaScript</li>
-            <li>TypeScript</li>
-            <li>Java</li>
-          </ul>
-        </div>
-        <div>
-          <h3>Technologies/Frameworks</h3>
-          <ul>
-            <li>React.js</li>
-            <li>Next.js</li>
-            <li>CSS</li>
-            <li>Tailwind</li>
-            <li>Node.js</li>
-          </ul>
-        </div>
-        <div>
-          <h3>Tools</h3>
-          <ul>
-            <li>Git</li>
-            <li>VS Code</li>
-          </ul>
-        </div>
+      <div className="">
+        <ul className="grid-container gap-5">
+          {skillItems.map(({ name, Icon }) => {
+            return (
+              <li
+                key={name}
+                className="grid-item group border border-transparent hover:border-light/40 transition duration-500"
+              >
+                <div className="saturate-[0.25] group-hover:saturate-100">
+                  <Icon />
+                </div>
+                {name}
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </div>
   );
