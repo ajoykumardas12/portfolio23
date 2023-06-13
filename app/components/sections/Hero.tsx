d3c816cc5838cd2e95116efd6abe23844f001ef9
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-06-13 12:24:22 +0530

    adjust hero top margin

--- app/components/sections/Hero.tsx
+++ app/components/sections/Hero.tsx
@@ -3,7 +3,7 @@ import SocialIcons from "../SocialIcons";
 
 function Hero() {
   return (
-    <section className="w-[87%] xs:w-[85%] sm:w-[82%] lg:w-[78%] xl:w-9/12 min-h-[calc(100vh-5rem)] flex items-center mt-20">
+    <section className="w-[87%] xs:w-[85%] sm:w-[82%] lg:w-[78%] xl:w-9/12 min-h-[calc(100vh-5rem)] flex items-center mt-16 md:mt-20">
       <div className="relative">
         <div className="w-max flex px-4 py-1 bg-gradient-to-r from-brand/50 to-[#0a468b] border border-highlight/40 rounded-r-2xl rounded-tl-2xl mb-6">
           <span className="text-xl md:text-2xl font-medium">नमस्ते</span>
