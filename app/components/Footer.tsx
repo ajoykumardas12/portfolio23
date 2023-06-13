77ff9ff398664e04cc43107459e99d97743282a8
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-06-13 09:45:36 +0530

    adjust footer padding

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 function Footer() {
   return (
-    <footer className="w-full flex items-center justify-center p-6">
+    <footer className="w-full flex items-center justify-center p-6 sm:pt-10 md:pt-12 lg:pt-16">
       <div className="text-xs xs:text-sm font-extralight text-center">
         Designed and developed with &#10084; by{" "}
         <Link href="/" scroll={false}>
