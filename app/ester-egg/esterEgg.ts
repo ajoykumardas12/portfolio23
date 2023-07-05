fced882c798cabb76bf3f9c98d0c8d8b2a6d881e
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-07-05 09:02:36 +0530

    add encryption puzzle in console

--- app/ester-egg/esterEgg.ts
+++ app/ester-egg/esterEgg.ts
@@ -9,6 +9,22 @@ function printEsterEgg() {
     o888o   o888o o888o     .8'      Y888""8o Y8P 
                         .o..P                  '  
                         'Y8P'                     `);
+  const puzzle = "Hello_World";
+  const encrypted = puzzle
+    .split("")
+    .map((char) => String.fromCharCode(char.charCodeAt(0) + 5))
+    .join("");
+
+  console.log("Solve this encryption:");
+  console.log(`%cEncrypted: ${encrypted}`, "font-size: 1.2rem;");
+
+  // Solution: Replace each character with the one 5 positions before in the ASCII table
+  // const decrypted = encrypted
+  //   .split("")
+  //   .map((char) => String.fromCharCode(char.charCodeAt(0) - 5))
+  //   .join("");
+
+  // console.log(`Decrypted: ${decrypted}`);
 }
 
 export default printEsterEgg;
