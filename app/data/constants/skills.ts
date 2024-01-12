d01254abf43abb9caba1405bb4f114c17dc57746
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2024-01-12 11:36:25 +0530

    feat: using constants for skills

---
+++ app/data/constants/skills.ts
@@ -0,0 +1,52 @@
+import CSSIcon from "@/app/components/icons/skills/CSSIcon";
+import Git from "@/app/components/icons/skills/Git";
+import Java from "@/app/components/icons/skills/Java";
+import JavaScript from "@/app/components/icons/skills/JavaScript";
+import NextJS from "@/app/components/icons/skills/NextJS";
+import NodeJS from "@/app/components/icons/skills/NodeJS";
+import ReactIcon from "@/app/components/icons/skills/ReactIcon";
+import TailwindCSS from "@/app/components/icons/skills/TailwindCSS";
+import TypeScript from "@/app/components/icons/skills/TypeScript";
+import VSCode from "@/app/components/icons/skills/VSCode";
+
+const skillItems = [
+  { name: "JavaScript", Icon: JavaScript },
+  {
+    name: "TypeScript",
+    Icon: TypeScript,
+  },
+  {
+    name: "React",
+    Icon: ReactIcon,
+  },
+  {
+    name: "Next.js",
+    Icon: NextJS,
+  },
+  {
+    name: "CSS",
+    Icon: CSSIcon,
+  },
+  {
+    name: "Tailwind",
+    Icon: TailwindCSS,
+  },
+  {
+    name: "Node.js",
+    Icon: NodeJS,
+  },
+  {
+    name: "Java",
+    Icon: Java,
+  },
+  {
+    name: "Git",
+    Icon: Git,
+  },
+  {
+    name: "VS Code",
+    Icon: VSCode,
+  },
+];
+
+export default skillItems;
