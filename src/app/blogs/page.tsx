42ab352d1f6d9713ed9c153657dc4cd7202e885f
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2024-04-04 23:51:23 +0530

    feat: basic responsiveness

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -12,9 +12,9 @@ export default function Blog() {
   const posts = getPostsData();
 
   return (
-    <div className="w-10/12 mx-auto">
+    <div className="w-11/12 mx-auto">
       <h1 className="text-3xl mb-10">Latest Posts</h1>
-      <div className="grid grid-cols-3 gap-12 justify-items-center">
+      <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-12 justify-items-center">
         {posts.map((post) => {
           const slug = post.slug;
           return <BlogPostCard key={slug} slug={slug} />;
